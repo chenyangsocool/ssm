02c4c70559e6afcf995b9cd629592191af0f925a
chenyang <346103239@qq.com>
2018-03-15 08:39:49 +0800

    修改了swagger中作者信息的调用方式

--- src/main/java/com/chenyangsocool/ssm/tools/swagger/Swagger2Config.java
+++ src/main/java/com/chenyangsocool/ssm/tools/swagger/Swagger2Config.java
@@ -1,5 +1,4 @@
 package com.chenyangsocool.ssm.tools.swagger;
-import io.swagger.annotations.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,11 +28,13 @@ public class Swagger2Config {
                 .build();
     }
     private ApiInfo apiInfo() {
+        Contact contact=new Contact("Young","http://www.cnblogs.com/chenyangsocool","346103239@qq.com");
         return new ApiInfoBuilder()
                 .title("SSM RESTful APIs")
                 .description("This API Document is based on RESTful Style, The description is detail and auto-generation, It's very friendly for developers.")
                 .termsOfServiceUrl("http://www.cnblogs.com/chenyangsocool/")
-                .contact("Young")
+                //.contact("Young")//旧版的作者信息，建议用新版
+                .contact(contact)
                 .version("1.0.0")
                 .build();
     }
