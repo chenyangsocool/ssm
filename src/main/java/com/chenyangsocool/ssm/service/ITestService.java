f8d8a0309747b04b09b87afe9edad996695ff80d
chenyang <346103239@qq.com>
2018-03-15 11:21:39 +0800

    集成了WebService服务

--- src/main/java/com/chenyangsocool/ssm/service/ITestService.java
+++ src/main/java/com/chenyangsocool/ssm/service/ITestService.java
@@ -2,6 +2,9 @@ package com.chenyangsocool.ssm.service;
 
 import com.chenyangsocool.ssm.model.Test;
 
+import javax.jws.WebService;
+
+@WebService
 public interface ITestService {
     public Test getModelById(int id);
 }
